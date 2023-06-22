3a5417bb60661b0a8f698fca874799759771fc7c
Matias Britez <matiasbritez88@gmail.com>
2023-06-22 15:04:12 -0300

    prueba proyecto react 2

--- src/Components/ItemList/ItemList.jsx
+++ src/Components/ItemList/ItemList.jsx
@@ -1,20 +1,10 @@
 //Es un agrupador de un set de componentes Item.js (Deberías incluirlo dentro de ItemListContainer de la primera pre-entrega del Proyecto Final)
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 import { Link } from "react-router-dom";
 import "../ItemList/ItemList.css";
-
 import Item from "../Item/Item";
 
-const ItemList = () => {
-  const [prods, setProds] = useState([]);
-
-  useEffect(() => {
-    axios(`https://rickandmortyapi.com/api/character`).then((json) =>
-      setProds(json.data.results)
-    );
-  }, []);
-
+const ItemList = ({ prods }) => {
   return (
     <div className="Card-List">
       {prods.map((prod) => {
--- src/Components/ItemListContainer/ItemListContainer.jsx
+++ src/Components/ItemListContainer/ItemListContainer.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./ItemListContainer.css";
 import ItemList from "../ItemList/ItemList";
 
 const ItemListContainer = () => {
+  const [prods, setProds] = useState([]);
+
+  useEffect(() => {
+    axios(`https://rickandmortyapi.com/api/character`).then((json) =>
+      setProds(json.data.results)
+    );
+  }, []);
+
   return (
     <div>
       <h1>ItemListContainer</h1>
-      <ItemList />
+      <ItemList prods={prods} />
     </div>
   );
 };
